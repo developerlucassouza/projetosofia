795e870bb9768afef83e8e33d3fa5f9ec938d496
Lucas Souza <107756390+developerlucassouza@users.noreply.github.com>
2026-04-10 16:08:09 -0300

    Initial commit

---
+++ dados.js
@@ -0,0 +1,93 @@
+dados = [
+    // ADA E LEON - RESIDENT EVIL 4
+    {
+        "casal": "Ada e Leon",
+        "obra": "Resident Evil 4",
+        "foto": "adaeleon-residentevil4.png",
+        "texto": "TEXTO AQUI"
+    },
+    // ANDIE E BENJAMIN - COMO PERDER UM HOMEM EM 10 DIAS
+    {
+        "casal": "Andie e Benjamin",
+        "obra": "Como Perder um Homem em 10 Dias",
+        "foto": "andieebenjamin-comoperderumhomemem10dias.png",
+        "texto": "TEXTO AQUI"
+    },
+    // ARIEL E ERIC - A PEQUENA SEREIA
+    {
+        "casal": "Ariel e Príncipe Eric",
+        "obra": "A Pequena Sereia",
+        "foto": "arieleeric-apequenasereia.png",
+        "texto": "TEXTO AQUI"
+    },
+    // AURORA E PHILLIP - A BELA ADORMECIDA
+    {
+        "casal": "Aurora e Príncipe Phillip",
+        "obra": "A Bela Adormecida",
+        "foto": "auroraephillip-abelaadormecida.png",
+        "texto": "TEXTO AQUI"
+    },
+    // BELLA E EDWARD - CREPUSCULO
+    {
+        "casal": "Bella e Edward",
+        "obra": "Crepúsculo",
+        "foto": "bellaeedward-crepusculo.png",
+        "texto": "TEXTO AQUI"
+    },
+    // JUDY E NICK - ZOOTOPIA
+    {
+        "casal": "Judy e Nick",
+        "obra": "Zootopia",
+        "foto": "judyenick-zootopia.png",
+        "texto": "TEXTO AQUI"
+    },
+    // KAGUYA E SHIROGANE - KAGUYA-SAMA
+    {
+        "casal": "Kaguya e Shirogane",
+        "obra": "Kaguya-sama: Love is War",
+        "foto": "kaguyaeshirogane-kaguyasama.png",
+        "texto": "TEXTO AQUI"
+    },
+    // KATARA E AANG - AVATAR
+    {
+        "casal": "Katara e Aang",
+        "obra": "Avatar: A Lenda de Aang",
+        "foto": "kataraeaang-avatar.png",
+        "texto": "TEXTO AQUI"
+    },
+    // LOUISA E WILL - COMO EU ERA ANTES DE VOCÊ
+    {
+        "casal": "Louisa e Will",
+        "obra": "Como Eu Era Antes de Você",
+        "foto": "louisaewill-comoeueraantesdevoce.png",
+        "texto": "TEXTO AQUI"
+    },
+    // PADMÉ E ANAKIN - STAR WARS
+    {
+        "casal": "Padmé e Anakin",
+        "obra": "Star Wars III",
+        "foto": "padmeeanakin-starwars3.png",
+        "texto": "TEXTO AQUI"
+    },
+    // RAPUNZEL E JOSÉ - ENROLADOS
+    {
+        "casal": "Rapunzel e José",
+        "obra": "Enrolados",
+        "foto": "rapunzelejose-enrolados.png",
+        "texto": "TEXTO AQUI"
+    },
+    // SARA E MICHAEL - AMOR DE REDENÇÃO
+    {
+        "casal": "Sara e Michael",
+        "obra": "Amor de Redenção",
+        "foto": "saraemichael-amorderedencao.png",
+        "texto": "TEXTO AQUI"
+    },
+    // TIANA E NAVEEN - A PRINCESA E O SAPO
+    {
+        "casal": "Tiana e Príncipe Naveen",
+        "obra": "A Príncesa e o Sapo",
+        "foto": "tianaenaveen-aprincesaeosapo.png",
+        "texto": "TEXTO AQUI"
+    }
+]

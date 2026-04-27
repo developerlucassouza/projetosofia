6928b603df36a84689af94e8b86acec627a4ad0e
Lucas Souza <107756390+developerlucassouza@users.noreply.github.com>
2026-04-27 11:40:44 -0300

    up

--- dados.js
+++ dados.js
@@ -46,14 +46,14 @@ dados = [
         "casal": "Kaguya e Shirogane",
         "obra": "Kaguya-sama: Love is War",
         "foto": "kaguyaeshirogane-kaguyasama.png",
-        "texto": "TEXTO AQUI"
+        "texto": "Sabe o que faz eu gostar tanto desse casal? Além da Kaguya ser extremamente parecida com você? A forma como eles se amam, de um lado a gente tem uma herdeira de familia bilionária, genial, habilidosa em vários esportes, e do outro um dos garotos mais geniais (se não, o mais genial) do Japão, extremamente esforçado, e que faz de tudo para provar que consegue fazer de tudo para atingir seus objetivos. Porém, eles não se amam por nenhuma dessas qualidade, eles amam os defeitos e fraquezas um do outro, eles amam exatamente aquilo que tentam esconder um do outro e de todos. Kaguya vê o Shirogane esforçado e corajoso, mas também vê o Shirogane envergonhado, tímido, e como o seu esforço busca esconder as suas fraquezas e defeitos, e ela ama esse lado que ele tenta esconder de todos, eu quero que sempre sejamos assim, que não nos ame-mos APESAR dos nossos defeitos e problemas, mas que eles sejam sempre parte do que amamos um no outro. É por isso que eu amo você, por você sempre cuidar dos meus problemas, por eu sempre poder ser eu mesmo com você, não importa o quão inseguro ou assustado eu esteja, você está comigo, eu sempre estarei com você."
     },
     // KATARA E AANG - AVATAR
     {
         "casal": "Katara e Aang",
         "obra": "Avatar: A Lenda de Aang",
         "foto": "kataraeaang-avatar.png",
-        "texto": "TEXTO AQUI"
+        "texto": "Aang era um garoto de 12 anos com uma missão extremamente simples: parar uma guerra que dura por 100 anos kkkkkkkkk coisa boba, todo garoto de 12 anos consegue. Apesar de ele SER o Avatar, convenhamos que isso é sim algo impossível para um garoto de 12 anos, mas mesmo assim ele conseguiu, porque ele tinha alguém que sempre o apoiava. Katara sempre acreditou no Aang, mesmo quando o Aang estava fraco, com medo, incapaz de lutar, Katara sabia que ele era o Avatar e era totalmente capaz de acabar com a guerra, mesmo parecendo impossível, Aang jamais venceria sem a Katara. E é isso que você faz comigo, você acredita em mim quando nem eu acredito em mim mesmo, você me faz acreditar que consigo fazer qualquer coisa, você é o suporte que tanto preciso para continuar lutando as batalhas do dia a dia, assim como Katara sempre ajudou o Aang. É por isso que eu amo você, você sempre me da suporte, me da forças, e faz eu ser todo dia a melhor versão de mim mesmo, porque você faz eu acreditar que eu posso."
     },
     // LOUISA E WILL - COMO EU ERA ANTES DE VOCÊ
     {
@@ -67,21 +67,21 @@ dados = [
         "casal": "Padmé e Anakin",
         "obra": "Star Wars III",
         "foto": "padmeeanakin-starwars3.png",
-        "texto": "TEXTO AQUI"
+        "texto": "Anakin está longe de ser um herói, nem perto disso na verdade, mas uma coisa é fato: ele amava Padmé como jamais amou qualquer um, e estava disposto a fazer absolutamente tudo para protege-lá. No menor risco de perde-la, Anakin se dispos a destruir toda a sociedade que ele conhecia e servia apenas pela menor chance de salva-la. 'Eu te daria o mundo meu amor, mas ele é sujo demais para você. Eu queimaria ele inteiro para que você vive-se em um mundo melhor', você se tornou o meu mundo, sem você, nada mais faz sentido, nada mais importa, quero apenas uma vida ao seu lado e estou disposto a sacrificar tudo por isso, assim como Anakin sacrificou tudo para tentar salvar Padmé. É por isso que eu amo você, porque você é o meu mundo, e estou disposto a fazer tudo para salva-la."
     },
     // RAPUNZEL E JOSÉ - ENROLADOS
     {
         "casal": "Rapunzel e José",
         "obra": "Enrolados",
         "foto": "rapunzelejose-enrolados.png",
-        "texto": "TEXTO AQUI"
+        "texto": "'Meu nome mesmo é José Bezerra, uma hora você ia descobrir', 'Escuta, só para registrar, eu gosto muito mais de José Bezerra de que de Flynn Rider'. José nunca gostou de si mesmo, de quem ele era, então baseado em um personagem de livro, ele assume a identidade de Flynn Rider, em busca de ser como o personagem, fanfarrão, cheio da grana, sucesso com a mulherada, tudo o que ele queria ser, porém, negando quem ele realmente era, e viveu anos assim, vivendo um personagem ao qual ele não era, e sabia que nunca seria, mas ainda assim, era melhor que ser o José Bezerra, quem amaria o José Bezerra ao invés do Flynn Rider? Até conhecer a Rapunzel. Rapunzel conheceu o Flynn e o José, e preferiu mil vezes José, isso que faz eles especiais, José mostrou o mundo para Rapunzel e fez ela se encantar por tal, Rapunzel mostrou ele mesmo para ela, e fez ele gostar de si mesmo, assim como estar com você faz eu gostar de mim mesmo, deixar de ser 'Flynn Rider' e poder ser 'José Bezerra'. É por isso que eu amo você, porque com você, eu posso ser quem eu sou, sem medo, e sem julgamento."
     },
     // SARA E MICHAEL - AMOR DE REDENÇÃO
     {
         "casal": "Sara e Michael",
         "obra": "Amor de Redenção",
         "foto": "saraemichael-amorderedencao.png",
-        "texto": "TEXTO AQUI"
+        "texto": "Michael pedia constantemente a Deus para que lhe envia-se uma mulher com quem compartilhar a vida, com quem pudesse ficar junto, com quem pudesse viver feliz. O que ele não esperava, e que a mulher a quem ele orava era na verdade a maior prostituta de sua cidade (DEIXA EU TERMINAR, CALMA KKKKK). A mulher que Deus mandava a ele, não era perfeita, nem perto disso, era a pessoa mais problemática que o mundo já viu, Sara não conseguia confiar em Michael, ela nunca pode confiar em ninguém, principalmente nos homens. Ela sempre foi usada, e seu valor era apenas sobre o quanto ela poderia oferecer seus usos a ela. Quando Michael apareceu, nada fez sentido a ela, um homem que se negava constantemente a dormir com ela? Ele era louco? Não, apenas queria cuidar dela. A maior prostituta da cidade conheceu o amor, e aos poucos foi deixando de ser a pessoa problemática que era, e foi se tornando uma esposa modelo. Apesar de não acreditar que podia viver a altura de Michael, ela conseguiu, e era tudo o que ele queria, apenas ela. Sempre estive disposto a encontrar alguém problemática, sempre estive disposto a ser Michael, mas nunca percebi que eu poderia ser Sara. Eu e você somos problemáticos meu amor, vários traumas, inseguranças, e medos, ambos somos como Sara, com vários erros e decisões ruins em nossas vidas. Porém, da mesma forma que ambos somos como Sara, ambos somos como Michael, e estamos sempre dispostos a amar, perdoar, e cuidar um do outro, e mostrar ao outro que podemos sim viver a altura um do outro. É por isso que eu amo você, porque não sou Sara ou Michael, nem você, ambos somos problemáticos, e ambos cuidamos um do outro. Obrigado por cuidar de mim, e saiba que sempre irei cuidar de você."
     },
     // TIANA E NAVEEN - A PRINCESA E O SAPO
     {
